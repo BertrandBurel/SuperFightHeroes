98ebc0554a2bca445a0adcbe3bb74db1a2083591
ctoxy <cedric.bouve@gmail.com>
2018-04-05 16:35:43 +0200

    add component nav history game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GaleryComponent } from './galery/galery.component';
+import { GameComponent } from './game/game.component';
+import { HistoryComponent } from './history/history.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'app', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'home', component: GaleryComponent },
+  { path: 'home', component: GameComponent },
+  { path: 'home', component: HistoryComponent },
   ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GaleryComponent } from './galery/galery.component';
+import { GameComponent } from './game/game.component';
+import { HistoryComponent } from './history/history.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GaleryComponent
+    GaleryComponent,
+    GameComponent,
+    HistoryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
