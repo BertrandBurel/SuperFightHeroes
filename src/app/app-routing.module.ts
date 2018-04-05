3b06fa5e682ff2a1622237ff36fbd7821887b431
ctoxy <cedric.bouve@gmail.com>
2018-04-05 16:25:49 +0200

    add bootstrap jquery routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GaleryComponent } from './galery/galery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'app', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', component: GaleryComponent },
+  ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
