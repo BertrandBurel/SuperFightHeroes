7173bebfd2199c2c507cf67f3e899867ee685dc9
ctoxy <cedric.bouve@gmail.com>
2018-04-05 20:34:38 +0200

    galery test ok

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ApiService } from '../shared/api.service';
 
 @Component({
@@ -8,13 +7,37 @@ import { ApiService } from '../shared/api.service';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-
+  heroes; // all the heroes
+  hero; // hero par rapport a son id
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getHeroes().subscribe(heroes => {
-      console.log(heroes);
+      // console.log(heroes);
+      this.heroes = heroes;
     });
+    this.apiService.getHeroesId(12).subscribe(hero => {
+      console.log(hero);
+
+    });
+    this.apiService.getHeroesPower(13).subscribe(hero => {
+      console.log(hero);
+
+    });
+    this.apiService.getHeroesAppearance(14).subscribe(hero => {
+      console.log(hero);
+
+    });
+    this.apiService.getHeroesBiography(14).subscribe(hero => {
+      console.log(hero);
+
+    });
+    // this.apiService.getHeroesImageXs(this.heroes.slug).subscribe(hero => {
+    //   console.log(hero);
+
+    // });
+
+
   }
 
 }
